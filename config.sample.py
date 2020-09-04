369bc20823ce06e837319b985a9c5431d9bab349
hole-thu <hole_thu@riseup.net>
2020-09-04 22:03:22 +0800

    配置样例 README

---
+++ config.sample.py
@@ -0,0 +1,12 @@
+import random, string, time
+
+SQLALCHEMY_DATABASE_URI='sqlite:///hole.db'
+SQLALCHEMY_TRACK_MODIFICATIONS=False
+JSON_AS_ASCII=False
+CLIENT_ID='<id>'
+CLIENT_SECRET='<secret>'
+MASTODON_URL='https://thu.closed.social'
+REDIRECT_URI = 'http://hole.thu.monster/_auth'
+SALT = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
+ADMINS = ['cs_114514']
+START_TIME = int(time.time())

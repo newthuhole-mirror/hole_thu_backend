30bbadf57e3ad4a8bd19ba498c96465c7732ecd5
hole-thu <hole_thu@riseup.net>
2021-12-19 19:47:20 +0800

    调整热榜

--- hot_score_attenuation.py
+++ hot_score_attenuation.py
@@ -1,9 +1,13 @@
+import time
 from hole import app
 from models import Post, db
 
 with app.app_context():
     for p in Post.query.filter(
-        Post.hot_score > 0
+        Post.hot_score > 10
     ).all():
-        p.hot_score = int(p.hot_score * 0.9)
+        if time.time() - p.timestamp > 60 * 60 * 24 * 3:
+            p.hot_score = 10
+        else:
+            p.hot_score = int(p.hot_score * 0.9)
     db.session.commit()
